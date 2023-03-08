4ef80f4e85d157d6dfca206dd3b2767debfef84e
aspcodenet <stefan.holmberg@systementor.se>
2023-03-08 09:49:48 +0100

    aaaa

--- TucStefanDinner/App.cs
+++ TucStefanDinner/App.cs
@@ -1,8 +1,4 @@
-﻿
-
-using System.Xml.Linq;
-
-public class Person
+﻿public class Person : IGameObject
 {
     public static Random Random = new Random();
     public string Namn { get; set; }
@@ -15,14 +11,32 @@ public class Person
     }
 }
 
+public class Fly : IGameObject
+{
+    public static Random Random = new Random();
+    public void Act()
+    {
+        var items = new[] { "flyger", "surrar", "landar i maten" };
+        var latestAction = items[Random.Next(items.Length)];
+        Console.WriteLine($"Flugan {latestAction}");
+    }
+}
+
+
+public interface IGameObject
+{
+    void Act();
+}
+
 public class App
 {
     public void Run()
     {
-        var gameobjects = new List<Person>();
+        var gameobjects = new List<IGameObject>();
         gameobjects.Add(new Person{ Namn="Stefan" });
         gameobjects.Add(new Person { Namn = "Josefine" });
         gameobjects.Add(new Person { Namn = "Kerstin" });
+        gameobjects.Add(new Fly());
         while (true)
         {
             foreach (var gameobj in gameobjects)
