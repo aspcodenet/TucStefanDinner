fd6de5cc7f35d54ddb4034c20e2884ea339be09e
aspcodenet <stefan.holmberg@systementor.se>
2023-03-08 09:34:08 +0100

    Add project files.

---
+++ TucStefanDinner/App.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Xml.Linq;
+
+public class Person
+{
+    public static Random Random = new Random();
+    public string Namn { get; set; }
+
+    public void Act()
+    {
+        var items = new[] { "rapar", "äter", "dricker", "pratar" };
+        var latestAction = items[Random.Next(items.Length)];
+        Console.WriteLine($"{Namn} {latestAction}");
+    }
+}
+
+public class App
+{
+    public void Run()
+    {
+        var gameobjects = new List<Person>();
+        gameobjects.Add(new Person{ Namn="Stefan" });
+        gameobjects.Add(new Person { Namn = "Josefine" });
+        gameobjects.Add(new Person { Namn = "Kerstin" });
+        while (true)
+        {
+            foreach (var gameobj in gameobjects)
+            {
+                gameobj.Act();
+            }
+        }
+    }
+}
