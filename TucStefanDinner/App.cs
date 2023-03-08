089a28297116824ac5b84dde5a3f5d25f9b725ac
aspcodenet <stefan.holmberg@systementor.se>
2023-03-08 10:39:25 +0100

    aaa

--- TucStefanDinner/App.cs
+++ TucStefanDinner/App.cs
@@ -13,20 +13,46 @@
 
 public class Fly : IGameObject
 {
+    public int Level { get; set; } = 1;
+    public string LatestAction { get; set; }
     public static Random Random = new Random();
     public void Act()
     {
         var items = new[] { "flyger", "surrar", "landar i maten" };
-        var latestAction = items[Random.Next(items.Length)];
-        Console.WriteLine($"Flugan {latestAction}");
+        LatestAction = items[Random.Next(items.Length)];
+        Console.WriteLine($"Flugan {LatestAction}");
+    }
+
+    public void CheckIfLevelUp()
+    {
+        if (LatestAction == "landar i maten")
+            Level++;
     }
 }
 
 
+// SPECIFIKATION, LÖFTE, KONTRAKT 
+// i Sverige tvp oinnar på el-plugg
 public interface IGameObject
 {
     void Act();
+    void CheckIfLevelUp();
 }
+
+public class Cat : IGameObject
+{
+    public void Act()
+    {
+        Console.WriteLine("Katten sover");       
+    }
+
+    public void CheckIfLevelUp()
+    {
+    }
+}
+
+//1, Lägg till Katt
+//2. Level up!!! - algoritm
 
 public class App
 {
@@ -37,11 +63,18 @@ public class App
         gameobjects.Add(new Person { Namn = "Josefine" });
         gameobjects.Add(new Person { Namn = "Kerstin" });
         gameobjects.Add(new Fly());
+        var cat = new Cat();
+        gameobjects.Add(cat);
+
         while (true)
         {
             foreach (var gameobj in gameobjects)
             {
                 gameobj.Act();
+            }
+            foreach (var gameobj in gameobjects)
+            {
+                gameobj.CheckIfLevelUp();
             }
         }
     }
